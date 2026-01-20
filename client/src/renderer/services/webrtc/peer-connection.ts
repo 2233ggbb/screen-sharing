d0647e1852b94bf357bae1fa5a4ed128a3a30d3d
2233ggbb <1973233851@qq.com>
2026-01-21 00:07:46 +0800

    feat: 测试

--- client/src/renderer/services/webrtc/peer-connection.ts
+++ client/src/renderer/services/webrtc/peer-connection.ts
@@ -154,10 +154,10 @@ export class PeerConnectionManager {
         });
         
         // TODO: 测试 NAT 穿透时取消注释以下代码块，过滤掉 host 候选
-        // if (candidateType === 'host') {
-        //   console.warn('[ICE] 🚫 模拟非局域网环境：丢弃 host 候选', event.candidate.address);
-        //   return;
-        // }
+        if (candidateType === 'host') {
+          console.warn('[ICE] 🚫 模拟非局域网环境：丢弃 host 候选', event.candidate.address);
+          return;
+        }
 
         if (handlers.onIceCandidate) {
           console.log('[ICE] 调用 onIceCandidate 回调发送候选...');
